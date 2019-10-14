a9c1c8e9212b7e995f25ac44d1bb02fcc4ff85fd
kv40 <55906906+kv40@users.noreply.github.com>
2019-10-14 17:09:02 -0400

    Updates

---
+++ precache-manifest.125f964e2f68fec4beba5027140adb82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4d945461205eebae8a7cf72c11e38c0",
+    "url": "/react_calculator/index.html"
+  },
+  {
+    "revision": "b6ee7fff8d917198c49d",
+    "url": "/react_calculator/static/css/main.331e7dfe.chunk.css"
+  },
+  {
+    "revision": "13eee97cba9118f30101",
+    "url": "/react_calculator/static/js/2.a669c9ca.chunk.js"
+  },
+  {
+    "revision": "b6ee7fff8d917198c49d",
+    "url": "/react_calculator/static/js/main.1645fe56.chunk.js"
+  },
+  {
+    "revision": "f5dd3b73104dae565173",
+    "url": "/react_calculator/static/js/runtime-main.e4732de4.js"
+  }
+]);
