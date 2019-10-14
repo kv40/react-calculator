788fa5f5b766417d94fdaca4ad161e52c6b88861
kv40 <55906906+kv40@users.noreply.github.com>
2019-10-14 14:59:25 -0400

    Adding node and build

---
+++ build/precache-manifest.a8c1f646838b392fec91ed25c424f71d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34e404a40c33518ccfafb31376308565",
+    "url": "/React_Calculator/index.html"
+  },
+  {
+    "revision": "2a70f831a04e1b123bad",
+    "url": "/React_Calculator/static/css/main.affd0d07.chunk.css"
+  },
+  {
+    "revision": "13eee97cba9118f30101",
+    "url": "/React_Calculator/static/js/2.a669c9ca.chunk.js"
+  },
+  {
+    "revision": "2a70f831a04e1b123bad",
+    "url": "/React_Calculator/static/js/main.2968dfdd.chunk.js"
+  },
+  {
+    "revision": "fb71dbc08070f581f661",
+    "url": "/React_Calculator/static/js/runtime-main.f8ec5d4a.js"
+  }
+]);
